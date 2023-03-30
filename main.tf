1203f7ef816dfa71fad0e180f923eb915a1dff3d
Bilal Khan <bilalstanz@gmail.com>
2023-03-30 09:12:48 +0530

    Add provider config

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ variable "vcenter_pass" {
 
 
 
+
+
+
+
+provider "vsphere" {
+  user = var.vcenter_id
+  password = var.vcenter_pass
+  vsphere_server = "vcenter.indiqus.local"
+  allow_unverified_ssl = true
+}
+
