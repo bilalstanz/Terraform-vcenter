cb3295d63238d1d22f513fe2aa789da2d969465f
Bilal Khan <bilalstanz@gmail.com>
2023-03-30 09:16:53 +0530

    Create local variables and gitignore file

--- main.tf
+++ main.tf
@@ -8,8 +8,46 @@ variable "vcenter_pass" {
 }
 
 
+locals {
+  template_ub18_ip = "172.31.29.61"
+  # template_ub18_ip = "172.31.29.20"
+  template_ub20_ip = "172.31.29.71"
+  # host6_template_ub18_ip = "172.31.29.20"
 
+  ip1 = "172.31.29.62"
+  ip2 = "172.31.29.63"
+  ip3 = "172.31.29.64"
+  ip4 = "172.31.29.65"
+  ip5 = "172.31.29.66"
+  ip6 = "172.31.29.67"
+  ip7 = "172.31.29.68"
+  ip8 = "172.31.29.69"
 
+  octets1 = split(".", local.ip1)
+  last_2_octets1 = join(".", slice(local.octets1, 2, 4))
+
+  octets2 = split(".", local.ip2)
+  last_2_octets2 = join(".", slice(local.octets2, 2, 4))
+
+  octets3 = split(".", local.ip3)
+  last_2_octets3 = join(".", slice(local.octets3, 2, 4))
+
+  octets4 = split(".", local.ip4)
+  last_2_octets4 = join(".", slice(local.octets4, 2, 4))
+
+  octets5 = split(".", local.ip5)
+  last_2_octets5 = join(".", slice(local.octets5, 2, 4))
+
+  octets6 = split(".", local.ip6)
+  last_2_octets6 = join(".", slice(local.octets6, 2, 4))
+
+  octets7 = split(".", local.ip7)
+  last_2_octets7 = join(".", slice(local.octets7, 2, 4))
+
+  octets8 = split(".", local.ip8)
+  last_2_octets8 = join(".", slice(local.octets8, 2, 4))
+
+}
 
 
 
